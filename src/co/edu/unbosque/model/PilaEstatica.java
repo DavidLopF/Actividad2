3214bd686f6a050625fe0bfb8685aa833c6a5361
gitForDavidL <71913304+gitForDavidL@users.noreply.github.com>
2021-03-04 20:36:58 -0500

    interfaz pila creada

---
+++ src/co/edu/unbosque/model/PilaEstatica.java
@@ -0,0 +1,33 @@
+package co.edu.unbosque.model;
+
+public class PilaEstatica implements IPilaEnteros {
+    @Override
+    public void push(int element) {
+
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int getTop() {
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfElements() {
+        return 0;
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
